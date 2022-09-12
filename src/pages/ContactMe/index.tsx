1aab7d7b896ec90bdac512dd3499ddacf0961d75
rafelis1997 <rafaelheros80@gmail.com>
2022-09-12 19:43:42 -0300

    fix email sent redirection

--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -24,24 +24,24 @@ export function ContactMe() {
   } = useForm<Inputs>()
 
   const contactFormSubmit: SubmitHandler<Inputs> = (data, e) => {
-    fetch('https://formsubmit.co/ajax/rafaelheros80@gmail.com', {
-      method: 'POST',
-      mode: 'no-cors',
+    // fetch('https://formsubmit.co/ajax/rafaelheros80@gmail.com', {
+    //   method: 'POST',
+    //   mode: 'no-cors',
 
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        name: 'FormSubmit',
-        message: "I'm from Devro LABS",
-        // eslint-disable-next-line prettier/prettier
-      })
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      // eslint-disable-next-line prettier/prettier
-      .catch((error) => console.log(error));
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     Accept: 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     name: 'FormSubmit',
+    //     message: "I'm from Devro LABS",
+    //     // eslint-disable-next-line prettier/prettier
+    //   })
+    // })
+    // //   .then((response) => response.json())
+    // //   .then((data) => console.log(data))
+    // //   // eslint-disable-next-line prettier/prettier
+    // //   .catch((error) => console.log(error));
 
     // const headers = {
     //   'Content-Type': 'text/plain',
@@ -51,14 +51,14 @@ export function ContactMe() {
     //   ...data,
     //   headers,
     // })
-    // axios.defaults.headers.post['Content-Type'] = 'application/json'
-    // axios
-    //   .post('https://formsubmit.co/ajax/rafaelheros80@gmail.com', {
-    //     name: 'FormSubmit',
-    //     message: "I'm from Devro LABS",
-    //   })
-    //   .then((response) => console.log(response))
-    //   .catch((error) => console.log(error))
+    axios.defaults.headers.post['Content-Type'] = 'application/json'
+    // eslint-disable-next-line prettier/prettier
+    axios.post('https://formsubmit.co/ajax/rafaelheros80@gmail.com', {
+        name: 'FormSubmit',
+        message: "I'm from Devro LABS",
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error))
     // console.log(data)
   }
 
