5e29e8f25354e754451590d9dbfdebdd2a9d4b05
rafelis1997 <rafaelheros80@gmail.com>
2022-09-12 19:33:52 -0300

    fix email sent redirection

--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -5,7 +5,61 @@ import {
   MailImage,
 } from './styles'
 
+import { useForm, SubmitHandler } from 'react-hook-form'
+import axios from 'axios'
+
+interface Inputs {
+  nameCompany: string
+  email: string
+  //   text: string
+  _next: string
+  _template: string
+}
+
 export function ContactMe() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<Inputs>()
+
+  const contactFormSubmit: SubmitHandler<Inputs> = (data, e) => {
+    fetch('https://formsubmit.co/ajax/rafaelheros80@gmail.com', {
+      method: 'POST',
+      mode: 'no-cors',
+
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        name: 'FormSubmit',
+        message: "I'm from Devro LABS",
+      }),
+    })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data))
+    //   .catch((error) => console.log(error))
+
+    // const headers = {
+    //   'Content-Type': 'text/plain',
+    // }
+
+    // await axios.post('https://formsubmit.co/rafaelheros80@gmail.com', {
+    //   ...data,
+    //   headers,
+    // })
+    // axios.defaults.headers.post['Content-Type'] = 'application/json'
+    // axios
+    //   .post('https://formsubmit.co/ajax/rafaelheros80@gmail.com', {
+    //     name: 'FormSubmit',
+    //     message: "I'm from Devro LABS",
+    //   })
+    //   .then((response) => console.log(response))
+    //   .catch((error) => console.log(error))
+    // console.log(data)
+  }
+
   return (
     <ContactMeContainer>
       <ContactMeContent>
@@ -13,32 +67,43 @@ export function ContactMe() {
           <img src="Mail-bro.svg" alt="" />
         </MailImage>
 
-        <ContactForm
-          action="https://formsubmit.co/rafaelheros80@gmail.com"
-          method="POST"
-        >
+        <ContactForm onSubmit={handleSubmit(contactFormSubmit)}>
           <h1>Contact Form</h1>
 
           <label htmlFor="fname">Name/Company:</label>
           <input
-            type="text"
-            id="fname"
-            name="Name/Company"
+            {...register('nameCompany', {
+              required: true,
+            })}
             autoFocus
-            required
+            id="fname"
+            type="text"
           />
           <label htmlFor="lname">Email:</label>
-          <input type="email" id="lname" name="Email" required />
-          <label htmlFor="tname">Content:</label>
-          <textarea id="tname" name="Content" rows={4} wrap="soft" required />
+          <input
+            {...register('email', {
+              required: true,
+            })}
+            id="lname"
+            type="email"
+          />
+          {/* <label htmlFor="tname">Content:</label>
+          <textarea
+            {...register('text', {
+              required: true,
+            })}
+            id="tname"
+            rows={4}
+            wrap="soft"
+          /> */}
 
           <input
-            type="hidden"
-            name="_next"
+            {...register('_next')}
             value="https://rafael-almeida.vercel.app/#/contact-me/email-sent"
+            type="hidden"
           ></input>
 
-          <input type="hidden" name="_template" value="box" />
+          <input {...register('_template')} value="box" type="hidden" />
 
           <button type="submit">Enviar</button>
         </ContactForm>
